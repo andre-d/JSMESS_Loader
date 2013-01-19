4d111026f2dc06461a6175783ab3f6dc1d8e0be8
Andre D <andre@andred.ca>
2013-01-18 23:58:31 -0500

    Replace tabs with spaces

--- messloader.js
+++ messloader.js
@@ -18,49 +18,49 @@ var holder = document.getElementById('canvasholder');
 holder.appendChild(newCanvas);
 
 var fetch_file = function(url, cb) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url, true);
-	xhr.responseType = "arraybuffer";
-	xhr.onload = function(e) {
-		var ints = new Int8Array(xhr.response);
-		cb(ints);
-	};
-	xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "arraybuffer";
+  xhr.onload = function(e) {
+    var ints = new Int8Array(xhr.response);
+    cb(ints);
+  };
+  xhr.send();
 };
 
 var Module = {
-	'arguments': ["coleco","-verbose","-rompath",".","-cart",gamename,"-window","-resolution","256x192","-nokeepaspect"],
-	print: (function() {
-		var element = document.getElementById('output');
-		return function(text) {
-			text = text.replace(/&/g, "&amp;");
-			text = text.replace(/</g, "&lt;");
-			text = text.replace(/>/g, "&gt;");
-			text = text.replace('\n', '<br>', 'g');
-			element.innerHTML += text + '<br>';
-		};
-	})(),
-	canvas: document.getElementById('canvas'),
-	noInitialRun: false,
-	preInit: function() {
-		// Load the downloaded binary files into the filesystem.
-		for (var bios_fname in bios_files) {
-			if (bios_files.hasOwnProperty(bios_fname)) {
-				Module['FS_createDataFile']('/', bios_fname, bios_files[bios_fname], true, true);
-			}
-		}
-		Module['FS_createDataFile']('/', gamename, game_file, true, true);
-	}
+  'arguments': ["coleco","-verbose","-rompath",".","-cart",gamename,"-window","-resolution","256x192","-nokeepaspect"],
+  print: (function() {
+    var element = document.getElementById('output');
+    return function(text) {
+      text = text.replace(/&/g, "&amp;");
+      text = text.replace(/</g, "&lt;");
+      text = text.replace(/>/g, "&gt;");
+      text = text.replace('\n', '<br>', 'g');
+      element.innerHTML += text + '<br>';
+    };
+  })(),
+  canvas: document.getElementById('canvas'),
+  noInitialRun: false,
+  preInit: function() {
+    // Load the downloaded binary files into the filesystem.
+    for (var bios_fname in bios_files) {
+      if (bios_files.hasOwnProperty(bios_fname)) {
+        Module['FS_createDataFile']('/', bios_fname, bios_files[bios_fname], true, true);
+      }
+    }
+    Module['FS_createDataFile']('/', gamename, game_file, true, true);
+  }
 };
 
 var update_countdown = function() {
   file_countdown -= 1;
   if (file_countdown === 0) {
     var headID = document.getElementsByTagName("head")[0];
-		var newScript = document.createElement('script');
-		newScript.type = 'text/javascript';
-		newScript.src = 'messcolecovision.js.gz';
-		headID.appendChild(newScript);
+    var newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.src = 'messcolecovision.js.gz';
+    headID.appendChild(newScript);
   }
 };
 
